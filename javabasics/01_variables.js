ebaaf55cc3a5237d9e17a7f0c257723b007609af
HarshVarshney7 <harshvarsshney2110@gmail.com>
2025-05-25 04:46:16 +0000

    variables

---
+++ javabasics/01_variables.js
@@ -0,0 +1,20 @@
+
+const accountId = 144553
+let accountEmail = "harsh@gmail.com"
+var accountPassword = "1234"
+accountCity = "Jaipur" // not a good way
+let accountState;//value is undefined
+
+// accountId = 2 // not allowed
+
+accountEmail = "harsh123@ggmail.com"
+accountPassword = "123"
+accountCity ="Bengaluru"
+
+// shortcut to print (log+Enter)
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+
+/*
+prefer not to use var (because of issue in block slope and functional scope)
+*/
